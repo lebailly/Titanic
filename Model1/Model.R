e6379c3adfd9e752f60e572de8ace9a80d1c0cef
Chris LeBailly <chris.lebailly@gmail.com>
2014-05-02 12:12:00 -0700

    Reworked feature scaling

--- Model1/Model.R
+++ Model1/Model.R
@@ -1,39 +1,28 @@
 #!/usr/bin/Rscript
 
-#ATTEN - Clean up!
-
 main <- function()
 {
-	ImportData()
+	ImportData(train_ratio=0.6,val_ratio=0.2)
 
-	#Feature Scaling (move to a function? Better implementatoin? ATTEN)
-	scale <- c(1,1,max(train$Age), max(train$Pclass))
-	names(scale) <- c('Constant','Sex','Age','Pclass') #DO this & above in one? ATTEN
-	train$Age <- train$Age/scale['Age']
-	train$Pclass <- train$Pclass/scale['Pclass']
-	cat('Feature Scaling Factors:\n')
-	print(scale)
+	scale <- ComputeScale(train)
+	train <- ScaleFeatures(train, scale)
 
-	train$Constant <- 1
 	X <- train[c('Constant','Sex','Age','Pclass')]
 	y <- train$Survived
 
 	cost <- CreateCost(X,y)
 	theta <- c(1.678795,  2.592295, -2.331269, -2.994373) #Find better way to cache this! (should be rep(0,4))
 	#My solution found optimial theta to be c(-0.3093811,  2.3394740, -0.5074464, -1.2551855)
-	theta <- optim(theta,cost)$par
+	#theta <- optim(theta,cost)$par
 
 	names(theta) <- c('Constant','Sex','Age','Pclass')
 
-	cat('Theta =\n')
-	print(theta)
+	print(list(Theta=theta, Scale=scale))
 
 	############  MAKES PREDICTIONS ##################
 
-	test$Age <- test$Age/scale['Age']
-	test$Pclass <- test$Pclass/scale['Pclass']
+	test <- ScaleFeatures(test, scale)
 
-	test$Constant <- 1
 	X2 <- test[c('Constant','Sex','Age','Pclass')]
 	y2 <- test$Survived
 
@@ -49,6 +38,9 @@ main <- function()
 	X2$Prediction <- predictions
 	X2$Actual <- y2 #ATTEN - Double check this in previous versions.
 	
+	X2 <- UnscaleFeatures(X2, scale)
+	X2$Constant <- NULL
+
 	print(X2[X2$Prediction != X2$Actual,])
 
 	pdf('Incorrect.pdf')
@@ -67,6 +59,31 @@ main <- function()
 	cat(sprintf('This model accurately predicts %.2f%% of the data \n',accuracy))
 }
 
+ComputeScale <- function(data)
+#Computes the scale for features.
+{
+	scale <- c(Constant=1,Sex=max(data[,'Sex']),Age=max(data[,'Age'] ),
+			Pclass=max(data[,'Pclass']))
+}
+
+ScaleFeatures <- function(data, scale)
+{
+	data[,'Sex'] <- data[,'Sex']/scale['Sex']
+	data[,'Age'] <- data[,'Age']/scale['Age']
+	data[,'Pclass'] <- data[,'Pclass']/scale['Pclass']
+
+	data
+}
+
+UnscaleFeatures <- function(data, scale)
+{
+	data[,'Sex'] <- data[,'Sex']*scale['Sex']
+	data[,'Age'] <- as.integer(data[,'Age']*scale['Age']) #ATTEN - Why some give decimal?
+	data[,'Pclass'] <- data[,'Pclass']*scale['Pclass']
+
+	data
+}
+
 ImportData <- function(source='../Data/train.csv',train_ratio=0.6,val_ratio=0.2)
 #Pre-condition: source is a filename where the data is located
 #Post-conditino: Reads data from source, labels males as 0 and females 1
@@ -82,6 +99,8 @@ ImportData <- function(source='../Data/train.csv',train_ratio=0.6,val_ratio=0.2)
 	data$Sex[data$Sex=='male'] <- 0
 	data$Sex[data$Sex=='female'] <- 1
 	data$Sex <- as.numeric(data$Sex)
+
+	data$Constant <- 1
 
 	train_max <- floor(nrow(data)*train_ratio)
 	test_max <- floor(nrow(data)*(train_ratio+val_ratio))

662daf392c33ef4abd97449add10d38c2724431c
Chris LeBailly <chris.lebailly@gmail.com>
2014-05-04 00:10:11 -0700

    Cleaned and restructured parts of code

--- Model1/Model.R
+++ Model1/Model.R
@@ -2,44 +2,41 @@
 
 main <- function()
 {
-	ImportData(train_ratio=0.6,val_ratio=0.2)
+	ImportData(train_ratio=0.5) 
 
-	scale <- c(Constant=1,Sex=max(train$Sex),Age=max(train$Age),
-		Pclass=max(train$Pclass))
-	train <- ScaleFeatures(train, scale)
-
-	X <- train[c('Constant','Sex','Age','Pclass')]
-	y <- train$Survived
-	lambda <- 0
-	cost <- MakeCostFunct(X,y,lambda)
-
-	theta <- c(1.678795,  2.592295, -2.331269, -2.994373) #rep(0,4)
-	names(theta) <- names(scale)
-	theta <- optim(theta,cost)$par #Find a way to cache this!
-	#My gradiant decent solution found optimial theta to be c(-0.3093811,  2.3394740, -0.5074464, -1.2551855)
-
-	print(list(Theta=theta, Scale=scale, Lambda=lambda))
-
-	test <- ScaleFeatures(test, scale)
+	theta <- TrainModel(train)
+	print(theta)
 	test <- MakePredictions(test,theta)
-	test <- UnscaleFeatures(test, scale)
 
 	ErrorAnalysis(test)
 }
 
+TrainModel <- function(train, lambda = 0)
+{
+	#scale is a vector containing scaling factors.
+	scale <- c(Constant=1,Sex=max(train$Sex),Age=max(train$Age),
+		Pclass=max(train$Pclass))
+	train$Sex <- train$Sex/scale['Sex']
+	train$Age <- train$Age/scale['Age']
+	train$Pclass <- train$Pclass/scale['Pclass']
+
+	X <- train[c('Constant','Sex','Age','Pclass')]
+	y <- train$Survived
+	cost <- MakeCostFunct(X,y,lambda)
+
+	theta <- c(Constant=0,Sex=0,Age=0,Pclass=0)
+	theta <- optim(theta,cost)$par #Find a way to cache this!
+	
+	theta/scale
+}
+
 MakePredictions <- function(test,theta)
 {
-	X2 <- test[c('Constant','Sex','Age','Pclass')]
-	f <- function(z) (t(theta) %*% as.numeric(z))[1,1]
-	g <- function(z) 1/(1+exp(-z))
+	f <- function(z) 1/(1+exp(-(t(theta) %*% as.numeric(z))[1,1]))
 
-	predictions <- apply(X2,1,f)
-	probs <- sapply(predictions,g)
-	predictions[predictions>=0] <- 1
-	predictions[predictions<0] <- 0
-
-	test$Psurvival <- probs
-	test$Prediction <- predictions
+	X <- test[c('Constant','Sex','Age','Pclass')]
+	test$Psurvival <- apply(X,1,f)
+	test$Prediction <-  as.numeric(test$Psurvival >= 0.5)
 
 	test
 }
@@ -58,25 +55,7 @@ ErrorAnalysis <- function(test)
 	cat(sprintf('This model accurately predicts %.2f%% of the data \n',accuracy))
 }
 
-ScaleFeatures <- function(data, scale)
-{
-	data$Sex <- data$Sex/scale['Sex']
-	data$Age <- data$Age/scale['Age']
-	data$Pclass <- data$Pclass/scale['Pclass']
-
-	data
-}
-
-UnscaleFeatures <- function(data, scale)
-{
-	data$Sex <- data$Sex*scale['Sex']
-	data$Age <- data$Age*scale['Age']
-	data$Pclass <- data$Pclass*scale['Pclass']
-
-	data
-}
-
-ImportData <- function(source='../Data/train.csv',train_ratio=0.6,val_ratio=0.2)
+ImportData <- function(source='../Data/train.csv',train_ratio=0.7)
 #Pre-condition: source is a filename where the data is located
 #Post-conditino: Reads data from source, labels males as 0 and females 1
 {
@@ -93,11 +72,9 @@ ImportData <- function(source='../Data/train.csv',train_ratio=0.6,val_ratio=0.2)
 	data$Constant <- 1
 
 	train_max <- floor(nrow(data)*train_ratio)
-	test_max <- floor(nrow(data)*(train_ratio+val_ratio))
 
 	train <<- data[1:train_max,]
-	val <<- data[(train_max+1):test_max,]
-	test <<- data[(test_max+1):nrow(data),]
+	test <<- data[(train_max+1):nrow(data),]
 }
 
 MakeCostFunct <- function(X,y,lambda=0)
@@ -107,7 +84,7 @@ MakeCostFunct <- function(X,y,lambda=0)
 	function(theta)
 	{
 		h <- function(z) 1/(1+exp(-t(theta) %*% as.numeric(z)))[1,1]
-		-sum(y*log(apply(X,1,h))+log(1-apply(X,1,h))*(1-y))/m+lambda*sum(theta[-1]^2)
+		-sum(y*log(apply(X,1,h))+log(1-apply(X,1,h))*(1-y))/m+lambda*sum(theta[-1]^2)/(2*m)
 	}
 }
 
